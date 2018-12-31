3732f30b9bbc93706fd6a9d388d3a26fd588d4c0
Luke Edwards <luke.edwards05@gmail.com>
2018-12-31 15:28:47 -0800

    import module logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-export default function () {
-	//
+export default function (max) {
+	var num, curr, prev;
+	var limit = max || 1;
+
+	function keep(key, value) {
+		curr[key] = value;
+		if (++num === limit) {
+			prev = curr;
+			reset(1);
+		}
+	}
+
+	function reset(isPartial) {
+		num = 0;
+		curr = Object.create(null);
+		isPartial || (prev=Object.create(null));
+	}
+
+	reset();
+
+	return {
+		clear: reset,
+		has: function (key) {
+      return curr[key] !== void 0 || prev[key] !== void 0;
+    },
+		get: function (key) {
+			var val = curr[key];
+			if (val !== void 0) return val;
+			if ((val=prev[key]) !== void 0) {
+				keep(key, val);
+				return val;
+			}
+		},
+		set: function (key, value) {
+			if (curr[key] !== void 0) {
+				curr[key] = value;
+			} else {
+				keep(key, value);
+			}
+		}
+	};
 }
