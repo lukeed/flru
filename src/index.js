5e7fd112c5aa4808f0babadc5c38bf7b21e5cbe0
Alexander Lichter <manniL@gmx.net>
2019-01-10 13:49:08 +0000

    chore: add eslint config

--- src/index.js
+++ src/index.js
@@ -1,43 +1,43 @@
 export default function (max) {
-	var num, curr, prev;
-	var limit = max || 1;
+  let num, curr, prev
+  const limit = max || 1
 
-	function keep(key, value) {
-		if (++num > limit) {
-			prev = curr;
-			reset(1);
-			++num;
-		}
-		curr[key] = value;
-	}
+  function keep(key, value) {
+    if (++num > limit) {
+      prev = curr
+      reset(1)
+      ++num
+    }
+    curr[key] = value
+  }
 
-	function reset(isPartial) {
-		num = 0;
-		curr = Object.create(null);
-		isPartial || (prev=Object.create(null));
-	}
+  function reset(isPartial) {
+    num = 0
+    curr = Object.create(null)
+    isPartial || (prev = Object.create(null))
+  }
 
-	reset();
+  reset()
 
-	return {
-		clear: reset,
-		has: function (key) {
-      return curr[key] !== void 0 || prev[key] !== void 0;
+  return {
+    clear: reset,
+    has(key) {
+      return curr[key] !== void 0 || prev[key] !== void 0
     },
-		get: function (key) {
-			var val = curr[key];
-			if (val !== void 0) return val;
-			if ((val=prev[key]) !== void 0) {
-				keep(key, val);
-				return val;
-			}
-		},
-		set: function (key, value) {
-			if (curr[key] !== void 0) {
-				curr[key] = value;
-			} else {
-				keep(key, value);
-			}
-		}
-	};
+    get(key) {
+      let val = curr[key]
+      if (val !== void 0) { return val }
+      if ((val = prev[key]) !== void 0) {
+        keep(key, val)
+        return val
+      }
+    },
+    set(key, value) {
+      if (curr[key] !== void 0) {
+        curr[key] = value
+      } else {
+        keep(key, value)
+      }
+    }
+  }
 }
--- test/index.js
+++ test/index.js
@@ -1,101 +1,101 @@
-const test = require('tape');
-const flru = require('../dist/flru');
+const test = require('tape')
+const flru = require('../dist/flru')
 
-const isObject = x => Object.prototype.toString.call(x, '[object Object]');
+const isObject = x => Object.prototype.toString.call(x, '[object Object]')
 
-test('exports', t => {
-	let keys = ['has', 'clear', 'get', 'set'];
-	t.is(typeof flru, 'function', 'exports a function');
+test('exports', (t) => {
+  const keys = ['has', 'clear', 'get', 'set']
+  t.is(typeof flru, 'function', 'exports a function')
 
-	let foo = new flru();
-	t.true(isObject(foo), 'works with `new` keyword');
-	keys.forEach(k => {
-		t.is(typeof foo[k], 'function', `~> has "${k}" method`);
-	});
+  const foo = new flru()
+  t.true(isObject(foo), 'works with `new` keyword')
+  keys.forEach((k) => {
+    t.is(typeof foo[k], 'function', `~> has "${k}" method`)
+  })
 
-	let bar = flru();
-	t.true(isObject(bar), 'works without `new` keyword');
-	keys.forEach(k => {
-		t.is(typeof bar[k], 'function', `~> has "${k}" method`);
-	});
+  const bar = flru()
+  t.true(isObject(bar), 'works without `new` keyword')
+  keys.forEach((k) => {
+    t.is(typeof bar[k], 'function', `~> has "${k}" method`)
+  })
 
-	t.end();
-});
+  t.end()
+})
 
-test('has()', t => {
-	let key = 'a';
-	let foo = flru();
+test('has()', (t) => {
+  const key = 'a'
+  const foo = flru()
 
-	foo.set(key, 1);
-	t.is(foo.has(key), true, '~> returns `true` when known');
-	t.is(foo.has('foobar'), false, '~> returns `false` when unknown');
+  foo.set(key, 1)
+  t.is(foo.has(key), true, '~> returns `true` when known')
+  t.is(foo.has('foobar'), false, '~> returns `false` when unknown')
 
-	t.end();
-});
+  t.end()
+})
 
-test('clear()', t => {
-	let foo = flru(3);
-	let keys = ['a', 'b', 'c'];
+test('clear()', (t) => {
+  const foo = flru(3)
+  const keys = ['a', 'b', 'c']
 
-	keys.forEach(foo.set);
+  keys.forEach(foo.set)
 
-	keys.forEach(k => {
-		t.true(foo.has(k), `(pre) key "${k}" exists`);
-	});
+  keys.forEach((k) => {
+    t.true(foo.has(k), `(pre) key "${k}" exists`)
+  })
 
-	t.is(foo.clear(), undefined, '~> clear() returns nothing');
+  t.is(foo.clear(), undefined, '~> clear() returns nothing')
 
-	keys.forEach(k => {
-		t.false(foo.has(k), `(post) key "${k}" unknown`);
-	});
+  keys.forEach((k) => {
+    t.false(foo.has(k), `(post) key "${k}" unknown`)
+  })
 
-	t.end();
-});
+  t.end()
+})
 
-test('ordering', t => {
-	let foo = flru(3);
-	const keys = (arr, bool) => t.is(arr.every(foo.has), bool, `[${arr.toString()}] ~> ${bool ? '' : 'un'}known`);
+test('ordering', (t) => {
+  const foo = flru(3)
+  const keys = (arr, bool) => t.is(arr.every(foo.has), bool, `[${arr.toString()}] ~> ${bool ? '' : 'un'}known`);
 
-	// A=[a,b,c]  &  S=[]
-	['a', 'b', 'c'].forEach(foo.set);
-	keys(['a', 'b', 'c'], true);
+  // A=[a,b,c]  &  S=[]
+  ['a', 'b', 'c'].forEach(foo.set)
+  keys(['a', 'b', 'c'], true)
 
-	t.pass(`>> set('d')`); // A=[d]  &  S=[a,b,c]
-	t.is(foo.set('d', 4), undefined, '~> returns nothing');
-	keys(['a', 'b', 'c', 'd'], true);
+  t.pass(`>> set('d')`) // A=[d]  &  S=[a,b,c]
+  t.is(foo.set('d', 4), undefined, '~> returns nothing')
+  keys(['a', 'b', 'c', 'd'], true)
 
-	foo.set('e', 5);
-	t.pass(`>> set('e')`);
-	// A=[d,e]  &  S=[a,b,c]
-	keys(['a', 'b', 'c', 'd', 'e'], true); // 2n - 1
+  foo.set('e', 5)
+  t.pass(`>> set('e')`)
+  // A=[d,e]  &  S=[a,b,c]
+  keys(['a', 'b', 'c', 'd', 'e'], true) // 2n - 1
 
-	foo.set('f', 6);
-	t.pass(`>> set('f')`);
-	// A=[d,e,f]  &  S=[a,b,c]
-	keys(['d', 'e', 'f'], true);
-	keys(['a', 'b', 'c'], true); //=> stale
+  foo.set('f', 6)
+  t.pass(`>> set('f')`)
+  // A=[d,e,f]  &  S=[a,b,c]
+  keys(['d', 'e', 'f'], true)
+  keys(['a', 'b', 'c'], true) //= > stale
 
-	foo.set('g', 7);
-	t.pass(`>> set('g')`);
-	// A=[g]  &  S=[d,e,f]
-	keys(['d', 'e', 'f', 'g'], true);
-	keys(['a', 'b', 'c'], false); // purged
+  foo.set('g', 7)
+  t.pass(`>> set('g')`)
+  // A=[g]  &  S=[d,e,f]
+  keys(['d', 'e', 'f', 'g'], true)
+  keys(['a', 'b', 'c'], false) // purged
 
-	t.pass(`>> get('e')`);
-	// A=[g,e]  &  S=[d,e,f]
-	t.is(foo.get('e'), 5, '~> returns item value');
-	keys(['d', 'e', 'f', 'g'], true); // unchanged, altho "e" exists twice
+  t.pass(`>> get('e')`)
+  // A=[g,e]  &  S=[d,e,f]
+  t.is(foo.get('e'), 5, '~> returns item value')
+  keys(['d', 'e', 'f', 'g'], true) // unchanged, altho "e" exists twice
 
-	foo.get('d');
-	t.pass(`>> get('d')`);
-	// A=[g,e,d]  &  S=[d,e,f]
-	keys(['g', 'e', 'd', 'f'], true);
+  foo.get('d')
+  t.pass(`>> get('d')`)
+  // A=[g,e,d]  &  S=[d,e,f]
+  keys(['g', 'e', 'd', 'f'], true)
 
-	foo.set('a', 1);
-	t.pass(`>> set('a'`);
-	// A=[a]  &  S=[g,e,d]
-	keys(['a', 'g', 'e', 'd'], true);
-	keys(['f'], false); // purged
+  foo.set('a', 1)
+  t.pass(`>> set('a'`)
+  // A=[a]  &  S=[g,e,d]
+  keys(['a', 'g', 'e', 'd'], true)
+  keys(['f'], false) // purged
 
-	t.end();
-});
+  t.end()
+})
