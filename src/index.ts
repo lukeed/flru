dc3d4d06d3f595162ae000d13fa3ae04dfba0b91
GitHub Ace <githubnext@users.noreply.github.com>
2026-01-16 02:31:32 +0000

    refactor: rewrite from JavaScript to TypeScript
    Co-authored-by: Luke Edwards <lukeed@users.noreply.github.com>

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+export interface flruCache<T = any> {
+	clear(isPartial?: boolean): void;
+	has(key: string): boolean;
+	get(key: string): undefined | T;
+	set(key: string, value: T): void;
+}
+
+export default function flru<T = any>(max?: number): flruCache<T> {
+	let num: number, curr: Record<string, T>, prev: Record<string, T>;
+	const limit = max || 1;
+
+	function keep(key: string, value: T): void {
+		if (++num > limit) {
+			prev = curr;
+			reset(true);
+			++num;
+		}
+		curr[key] = value;
+	}
+
+	function reset(isPartial?: boolean): void {
+		num = 0;
+		curr = Object.create(null);
+		isPartial || (prev = Object.create(null));
+	}
+
+	reset();
+
+	return {
+		clear: reset,
+		has: function (key: string): boolean {
+			return curr[key] !== void 0 || prev[key] !== void 0;
+		},
+		get: function (key: string): undefined | T {
+			const val = curr[key];
+			if (val !== void 0) return val;
+			const prevVal = prev[key];
+			if (prevVal !== void 0) {
+				keep(key, prevVal);
+				return prevVal;
+			}
+		},
+		set: function (key: string, value: T): void {
+			if (curr[key] !== void 0) {
+				curr[key] = value;
+			} else {
+				keep(key, value);
+			}
+		}
+	};
+}
