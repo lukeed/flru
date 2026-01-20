981f881f6919e8458b1a04ec690d006fe74d7411
GitHub Ace <githubnext@users.noreply.github.com>
2026-01-20 18:43:52 +0000

    refactor: migrate to TypeScript and update API

--- flru.d.ts
+++ flru.d.ts
@@ -1,10 +1,8 @@
 export interface flruCache<T = any> {
-  clear(isPartial: boolean): void;
-  has(key: string): boolean;
-  get(key: string): undefined | T;
-  set(key: string, value: T): void;
+	clear(isPartial?: boolean): void;
+	has(key: string): boolean;
+	get(key: string): undefined | T;
+	set(key: string, value: T): void;
 }
 
-declare const flru: <T = any>(max: number) => flruCache<T>;
-
-export default flru;
+export default function flru<T = any>(max?: number): flruCache<T>;
